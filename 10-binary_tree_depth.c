e016838b02ffa296b53783162180d0f284172984
thanks299 <thanksagbeble@gmail.com>
2024-03-26 06:13:47 -0700

    binary code

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -10,9 +10,8 @@
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	size_t depth = 0;
-	
-	if (tree == NULL)
-		return (0);
+		if (tree == NULL)
+			return (0);
 
 	while (tree->parent != NULL)
 	{
